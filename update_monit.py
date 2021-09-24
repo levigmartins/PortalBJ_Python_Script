f0926e0937830a70d7bb20ec793721fd0cb183af
Levi Martins <levi.gm99@gmail.com>
2021-09-24 20:42:22 -0300

    Login no portal + download do relatório correto

--- update_monit.py
+++ update_monit.py
@@ -1,14 +1,45 @@
 from requests.api import options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
+#################################################### SETTINGS #########################################################
 
 options = webdriver.ChromeOptions()
-
-preferences = {"download.default_directory": "C:/Users/levig/Downloads"}
-
+preferences = {"download.default_directory": "C:\\Users\\levig\\Downloads", "safebrowsing.enabled": "false"}
 options.add_experimental_option("prefs", preferences)
-
 driver = webdriver.Chrome('C:/bin/chromedriver.exe',chrome_options=options)
+email = "your_mail_here"
+password = "your_password_here"
 
+#################################################### DOWNLOAD #########################################################
+
+# Acessa a página de login do Portal
+# Access login page
+driver.get("https://id.brasiljunior.org.br/sign-in")
+
+# Procura o form de email, espera, para caso da página não ter sido carregada, e insere o email setado nas configs
+# Search for the email input, wait for page to fully load and inputs the setted mail
+mail_form = driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div:nth-child(1) > div.form-float-label-wrapper > input')
+sleep(1)
+mail_form.send_keys(email)
+
+# Procura o form de senha, espera, para caso da página não ter sido carregada, e insere a senha setado nas configs
+# Search for the password input, wait for page to fully load and inputs the setted password
+password_form = driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div:nth-child(2) > div.form-float-label-wrapper > input')
+sleep(1)
+password_form.send_keys(password)
+
+# Clica no botão de logar
+# Click on login button
+driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div.d-flex.justify-content-center > button').click()
+sleep(2)
+
+# Acessa a página de Relatórios do Portal e espera um pouco para ter certeza do carregamento da página
+# Acess "Relatórios" (Reports) page and waits for page to fully load
 driver.get("https://portal.brasiljunior.org.br/relatorios")
-driver.find_element(By.XPATH, "//*[@id='wrapper']/div[2]/div/div[2]/div[1]/div/div/div[4]/a[1]").click()
+sleep(2)
+
+# Clica no botão de download do relatório de 2021
+# Click on the 2021 report download button
+driver.find_element_by_css_selector('#wrapper > div.content > div > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(5) > a.btn.btn-primary.btn-sm').click()
