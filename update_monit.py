441dc20ab467cf6dd92c7edd73a76943c0b2739d
Levi Martins <levi.gm99@gmail.com>
2021-09-24 02:04:55 -0300

    Selenium abrindo o Portal BJ no Chrome

--- update_monit.py
+++ update_monit.py
@@ -1,11 +1,14 @@
-import requests
+from requests.api import options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-downloadUrl = 'https://uploads.brasiljunior.org.br/uploads/report/file/38950/ejs_202120210923-17253-kymigd.xlsx'
+options = webdriver.ChromeOptions()
 
-req = requests.get(downloadUrl)
-filename = req.url[downloadUrl.rfind('/')+1:]
+preferences = {"download.default_directory": "C:/Users/levig/Downloads"}
 
-with open("C:/Users/levig/Downloads/" + filename, 'wb') as f:
-    for chunk in req.iter_content(chunk_size=8192):
-        if chunk:
-            f.write(chunk)
+options.add_experimental_option("prefs", preferences)
+
+driver = webdriver.Chrome('C:/bin/chromedriver.exe',chrome_options=options)
+
+driver.get("https://portal.brasiljunior.org.br/relatorios")
+driver.find_element(By.XPATH, "//*[@id='wrapper']/div[2]/div/div[2]/div[1]/div/div/div[4]/a[1]").click()
