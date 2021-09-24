3393684a84781753c805c4bd704b243af9f20b08
Levi Martins <38568172+levigmartins@users.noreply.github.com>
2021-09-24 20:47:29 -0300

    Apagando alguns comentários

--- update_monit.py
+++ update_monit.py
@@ -14,32 +14,28 @@ password = "your_password_here"
 
 #################################################### DOWNLOAD #########################################################
 
-# Acessa a página de login do Portal
 # Access login page
 driver.get("https://id.brasiljunior.org.br/sign-in")
 
-# Procura o form de email, espera, para caso da página não ter sido carregada, e insere o email setado nas configs
 # Search for the email input, wait for page to fully load and inputs the setted mail
 mail_form = driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div:nth-child(1) > div.form-float-label-wrapper > input')
 sleep(1)
 mail_form.send_keys(email)
 
-# Procura o form de senha, espera, para caso da página não ter sido carregada, e insere a senha setado nas configs
 # Search for the password input, wait for page to fully load and inputs the setted password
 password_form = driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div:nth-child(2) > div.form-float-label-wrapper > input')
 sleep(1)
 password_form.send_keys(password)
 
-# Clica no botão de logar
 # Click on login button
 driver.find_element_by_css_selector('#__next > div > div > div > div > div.auth-box-action-block-ui > div > div.auth-box-action-body > form > div.d-flex.justify-content-center > button').click()
 sleep(2)
 
-# Acessa a página de Relatórios do Portal e espera um pouco para ter certeza do carregamento da página
 # Acess "Relatórios" (Reports) page and waits for page to fully load
 driver.get("https://portal.brasiljunior.org.br/relatorios")
 sleep(2)
 
-# Clica no botão de download do relatório de 2021
 # Click on the 2021 report download button
 driver.find_element_by_css_selector('#wrapper > div.content > div > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(5) > a.btn.btn-primary.btn-sm').click()
+
+#########################################################################################################################
